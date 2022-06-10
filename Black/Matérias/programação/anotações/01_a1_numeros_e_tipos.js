30cf8c4f0687fc9737b1a410fcbd8d79350e8e11
Pedro-Black <106837310+Pedro-Black@users.noreply.github.com>
2022-06-10 16:12:17 -0300

    Add files via upload

---
+++ Black/Matérias/programação/anotações/01_a1_numeros_e_tipos.js
@@ -0,0 +1,80 @@
+// Números, tipos de dados e operações matemáticas
+
+// Soma
+2 + 3
+
+// Subtração
+4 - 3
+
+// Multiplicação
+3 * 3
+
+//Divisão
+3 / 2
+
+//Divisão inteira
+Math.floor(3 / 2)
+Math.trunc(3 / 2)
+parseInt(3 / 2)
+
+// Resto da divisão
+3 % 2
+
+// Potência
+3 ** 2
+Math.pow(3, 2)
+
+// Tipos de dados
+typeof 5
+typeof 5.0
+typeof "5.0"
+typeof "5"
+
+// Operações e conversão entre tipos
+
+// Soma com tipos diferentes
+20 + 3
+"20" + 3
+20 + "3"
+"20" + "3"
+
+// Multiplicação com tipos diferentes
+20 * 3
+"20" * 3
+20 * "3"
+
+// Conversão explícita de tipos
+parseInt("20") + 3
+20 + parseInt("3")
+Number("20") + 3
+20 + Number("3")
+
+// Valor absoluto
+Math.abs(5 - 3)
+Math.abs(3 - 5)
+
+// Arredondamento
+parseFloat(Math.PI)
+parseFloat(Math.PI).toFixed(2)
+Math.PI.toFixed(2)
+parseFloat(Math.PI.toFixed(2))
+
+//Comprimento de strings
+"Araquari".length
+"2022".length
+
+typeof "5.0"
+typeof "5"
+
+// Operações e conversão entre tipos
+
+// Soma com tipos diferentes
+20 + 3
+"20" + 3
+20 + "3"
+"20" + "3"
+
+// Multiplicação com tipos diferentes
+20 * 3
+"20" * 3
+20 * "3"
